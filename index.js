6135abbf02170b1e63e60cd01d013d5cb850a630
zishanHow <111298322+zishanHow@users.noreply.github.com>
2023-03-10 23:31:03 +0600

    redesign the navbar & search input element

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const btn = document.getElementById('search-btn')
 
 const apiKey = "cdf764e"
-const movieInput = "star wars"
+const searchInputEl = document.getElementById("search-input")
 
 
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault()
+btn.addEventListener('click', () => {
+    // e.preventDefault()
     console.log("Button Clicked")
+
+    fetch(`https://www.omdbapi.com/?s=${searchInputEl.value}&apikey=${apiKey}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            let mainHtml = ``
+            // for(let item of data){
+            //     console.log(item)
+            // }
+
+            // document.getElementById('main').innerHTML = mainHtml
+        })
 })
 
 
 
-fetch(`https://www.omdbapi.com/?s=${movieInput}&apikey=${apiKey}`)
-.then(res => res.json())
-.then(data => {
-    // console.log(data.Search)
-    for(let item of data.Search){
-        console.log(item)
-    }
-})
