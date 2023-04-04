f5d8ba157ce7c6e93364565fde8fce172f576037
zishanHow <111298322+zishanHow@users.noreply.github.com>
2023-04-05 00:30:37 +0600

    getting movie from omdb API

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const btn = document.getElementById('search-btn')
 const apiKey = "cdf764e"
 const searchInputEl = document.getElementById("search-input")
 
-
+/* don't need it for time being(maybe not at all)
 
 btn.addEventListener('click', async() => {
     console.log("Button Clicked")
@@ -22,6 +22,37 @@ btn.addEventListener('click', async() => {
         console.log(imdbID)
     }
 })
+*/
+
+// OMDb API=>(getting movies from it)
+async function getMoviesFromAPI(movieName){
+
+    // getting multiple movie data with "s" query(mainly "imdbID")
+    const res = await fetch(`https://www.omdbapi.com/?s=${movieName}&apikey=${apiKey}`)
+    const data = await res.json()
     
+    // looping the Search result for "imdbID"
+    for(let id of data.Search){
+
+        // getting details of those movies with "i" query, and "imdbID"
+        const res = await fetch(`https://www.omdbapi.com/?i=${id.imdbID}&apikey=${apiKey}`)
+        const movieData = await res.json()
+        console.log(movieData)
+    }
+}
+
+getMoviesFromAPI('la la land')
+
+
+
+
+btn.addEventListener("click", ()=> {
+    searchChecking()
+})
+
     
+function searchChecking(){
+    let trimChecking = searchInputEl.value
     
+    console.log(trimChecking.replace(/\s+/g, ' '))
+}
