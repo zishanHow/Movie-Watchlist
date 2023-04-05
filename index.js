a8b41346149939c7387002bbdcb8e59eb2e23866
zishanHow <111298322+zishanHow@users.noreply.github.com>
2023-04-06 01:31:48 +0600

    add JS class,
    add JS class and shows movies to the DOM.

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const btn = document.getElementById('search-btn')
 const apiKey = "cdf764e"
 const searchInputEl = document.getElementById("search-input")
 
-
 // OMDb API=>(getting movies from it)
-async function getMoviesFromAPI(movieName) {
+async function getDataFromAPI(movieName) {
     // 1 getting multiple movie data with "s" query(mainly "imdbID")
     const res = await fetch(`https://www.omdbapi.com/?s=${movieName}&apikey=${apiKey}`)
     const data = await res.json()
@@ -16,7 +15,9 @@ async function getMoviesFromAPI(movieName) {
             // 2 getting details of those movies with "i" query, and "imdbID"
             const res = await fetch(`https://www.omdbapi.com/?i=${id.imdbID}&apikey=${apiKey}`)
             const movieData = await res.json()
-            console.log(movieData)
+            // console.log(movieData)
+            //calling render movie function with a argument. 
+            renderMovie(movieData)
         }
     }
 }
@@ -24,32 +25,48 @@ async function getMoviesFromAPI(movieName) {
 // getting movie name from search input.
 function searchMoviesByName(name) {
     name = (searchInputEl.value).replace(/\s+/g, ' ')
-    console.log(name)
-    getMoviesFromAPI(name)
+    // console.log(name)
+    getDataFromAPI(name)
 }
 
 btn.addEventListener("click", () => {
     searchMoviesByName()
+    renderMovie()
 })
 
 
-/* don't need it for time being(maybe not at all)
-
-btn.addEventListener('click', async() => {
-    console.log("Button Clicked")
-
-    // getting multiple movie data with "s" query.
-    const res = await fetch(`https://www.omdbapi.com/?s=${searchInputEl.value}&apikey=${apiKey}`)
-    const data = await res.json()
-    // console.log(data.Search)
-    
-    for(let item of data.Search){
-        // console.log(item.imdbID)
-
-        // getting details of those movies with "i" query.
-        const res = await fetch(`https://www.omdbapi.com/?i=${item.imdbID}&apikey=${apiKey}`)
-        const imdbID = await res.json()
-        console.log(imdbID)
+class Moveis{
+    constructor(data){
+        Object.assign(this, data)
     }
-})
-*/
+
+    getMoviesFromAPI(){
+        const {Title, Poster, Ratings, Runtime, Genre, Plot} = this
+        // console.log(Title, Poster, Ratings[0].Value, Runtime, Genre, Plot)
+        return `
+            <div class="grid-container" id="grid-container">
+                <img class="movie-poster" src="${Poster}" alt="">
+            
+                <h2 class="movie-title">${Title}</h2>
+                <i class="fa-solid fa-star"> ${Ratings[0].Value}</i>
+            
+                <p class="Runtime">${Runtime}</p>
+                <p class="movie-Genre">${Genre}</p>
+                <!-- <button class="watchList btn"><i class="fa-solid fa-plus"></i></button> -->
+                <p class="watchList">Watchlist
+                    <button class="btn"><i class="fa-solid fa-plus"></i>
+                    </button>
+                </p>
+                <p class="movie-plot">
+                    ${Plot}
+                </p>
+            </div>
+        `
+    }
+}
+
+function renderMovie(movieData){
+    const theMovie = new Moveis(movieData)
+    let showMovieToDOM = theMovie.getMoviesFromAPI()
+    document.getElementById('main').innerHTML += showMovieToDOM
+}
