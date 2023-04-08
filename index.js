ecda3982b452bd3a9319e22949ba7ab51753eec8
zishanHow <111298322+zishanHow@users.noreply.github.com>
2023-04-09 02:05:41 +0600

    big changes to the omdb API function.
    divide omdb API into two separate function, so that i can use both the function separately for added to watchlist.

--- index.js
+++ index.js
@@ -1,60 +1,79 @@
-const btn = document.getElementById('search-btn')
-
+const searchBtn = document.getElementById('search-btn')
 const apiKey = "cdf764e"
 const searchInputEl = document.getElementById("search-input")
 
-// OMDb API=>(getting movies from it)
-async function getDataFromAPI(movieName) {
-    // 1 getting multiple movie data with "s" query(mainly "imdbID")
+// handel all the "click" event
+document.addEventListener('click', (e) => {
+    if (e.target.id === "search-btn") {
+        searchMoviesByName()
+        reset()
+    } else if (e.target.id === "fa-plus") {
+        console.log(e.target.dataset.dataid)
+        // imdbMovieFrmAPI(e.target.dataset.dataid)
+    }
+})
+
+// OMDb API=>(getting movies from it) [cmt=> 1, and 2.]
+// 1 getting multiple movie data with "s" query(mainly "imdbID")
+async function imdbIdFromAPI(movieName) {
     const res = await fetch(`https://www.omdbapi.com/?s=${movieName}&apikey=${apiKey}`)
     const data = await res.json()
-
-    if (data.Search) {
-        // looping the Search result for "imdbID"
+    // calling the imdbMovieFrmAPI() with argument for movies
+    if (data.Response) {
+        // looping the Search result for every "imdbID"
         for (let id of data.Search) {
-            // 2 getting details of those movies with "i" query, and "imdbID"
-            const res = await fetch(`https://www.omdbapi.com/?i=${id.imdbID}&apikey=${apiKey}`)
-            const movieData = await res.json()
-            // console.log(movieData)
-            //calling render movie function with a argument. 
-            renderMovie(movieData)
+            imdbMovieFrmAPI(id.imdbID)
         }
-    }
+    } else { return null }
+}
+
+// 2 getting details of those movies with "i" query, and "imdbID"
+async function imdbMovieFrmAPI(movieI) {
+    const res = await fetch(`https://www.omdbapi.com/?i=${movieI}&apikey=${apiKey}`)
+    const movieData = await res.json()
+    // console.log(movieData)
+    renderMovie(movieData)
 }
 
 // getting movie name from search input.
-function searchMoviesByName(name) {
-    name = (searchInputEl.value).replace(/\s+/g, ' ')
-    // console.log(name)
-    getDataFromAPI(name)
+function searchMoviesByName() {
+    let name = (searchInputEl.value).trim()
+    imdbIdFromAPI(name)
 }
 
-btn.addEventListener("click", () => {
-    searchMoviesByName()
-    renderMovie()
-})
+function renderMovie(movie) {
+    const theMovie = new Moveis(movie)
+    let showMovieToDOM = theMovie.getMoviesFromAPI()
+    document.getElementById('movies-el').innerHTML += showMovieToDOM
+}
 
+function reset() {
+    document.getElementById('movies-el').innerHTML = ""
+}
 
-class Moveis{
-    constructor(data){
+/*======================
+        JS Class        
+    ======================*/
+class Moveis {
+    constructor(data) {
         Object.assign(this, data)
     }
 
-    getMoviesFromAPI(){
-        const {Title, Poster, Ratings, Runtime, Genre, Plot} = this
-        // console.log(Title, Poster, Ratings[0].Value, Runtime, Genre, Plot)
+    getMoviesFromAPI() {
+        const { Title, Poster, imdbRating, Runtime, Genre, Plot, imdbID } = this
         return `
-            <div class="grid-container" id="grid-container">
-                <img class="movie-poster" src="${Poster}" alt="">
+            <div class="grid-container">
+                <img class="movie-poster" src="${Poster}" alt="${Title}">
             
                 <h2 class="movie-title">${Title}</h2>
-                <i class="fa-solid fa-star"> ${Ratings[0].Value}</i>
+                <i class="fa-solid fa-star"> ${imdbRating}</i>
             
                 <p class="Runtime">${Runtime}</p>
                 <p class="movie-Genre">${Genre}</p>
                 <!-- <button class="watchList btn"><i class="fa-solid fa-plus"></i></button> -->
                 <p class="watchList">Watchlist
-                    <button class="btn"><i class="fa-solid fa-plus"></i>
+                    <button class="watchlist-btn">
+                        <i class="fa-solid fa-plus" id="fa-plus" data-dataid="${imdbID}"></i>
                     </button>
                 </p>
                 <p class="movie-plot">
@@ -64,9 +83,6 @@ class Moveis{
         `
     }
 }
-
-function renderMovie(movieData){
-    const theMovie = new Moveis(movieData)
-    let showMovieToDOM = theMovie.getMoviesFromAPI()
-    document.getElementById('main').innerHTML += showMovieToDOM
-}
+/*======================
+        End JS Class        
+    ======================*/
